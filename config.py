9e558e290bfd3ea81858095ce941804bddd4ac96
CaQtiml <lsivakorn@gmail.com>
2023-10-23 16:22:18 +0200

    update

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from dotenv import dotenv_values
 import os
 
 current_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "db.env") 
-config = dict(dotenv_values(current_path))
-DATABASE_USERNAME = config["DATABASE_USERNAME"]
-DATABASE_PASSWORD = config["DATABASE_PASSWORD"]
-DATABASE_HOST = config["DATABASE_URL"]
-DATABASE_NAME = config["DATABASE_NAME"]
+config_var = dict(dotenv_values(current_path))
+DATABASE_USERNAME = config_var["DATABASE_USERNAME"]
+DATABASE_PASSWORD = config_var["DATABASE_PASSWORD"]
+DATABASE_HOST = config_var["DATABASE_URL"]
+DATABASE_NAME = config_var["DATABASE_NAME"]
 DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5432/{DATABASE_NAME}"
 
 db_params = {
